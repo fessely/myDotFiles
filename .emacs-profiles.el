8d10d8671988d14a7ce8d2cdb343f6a9b81f9b79
fessely <fabien.essely@gmail.com>
2022-03-03 14:39:51 +0100

    nouvelle machine suite

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,4 +1,4 @@
 (("default" . ((user-emacs-directory . "~/myDotFiles/.emacs.default")))
- ("doom" . ((user-emacs-directory . "~/config/doom-emacs/.emacs.d")
+ ("doom" . ((user-emacs-directory . "~/myDotFiles/config/doom-emacs/.emacs.d")
             (env . (("DOOMDIR" . "/home/fabien/myDotFiles/.doom.d")))))
 )
